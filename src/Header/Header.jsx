845f9765e0904c4d2b6068ffbe64ab8be55d6ee8
FM-Akib <muntasirakib08@gmail.com>
2024-02-01 23:47:49 +0600

    navbar added

---
+++ src/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+import logo from '.././assets/Job Koi.png'
+
+const Header = () => {
+    return (
+        <nav className="navbar">
+            <img src={logo} alt=""/>
+            <div className='nav-items'>
+                <Link to="">Statistics</Link>
+                <Link to="">Applied jobs</Link>
+                <Link to="">Blog</Link>
+            </div>
+            <button className="apply-btn shadow-lg" >Start Appling</button>
+        </nav>
+    );
+};
+
+export default Header;
