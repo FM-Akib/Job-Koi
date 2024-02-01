46dbaa4aa612f9700e178434d5b4b695489847cd
FM-Akib <muntasirakib08@gmail.com>
2024-02-02 02:18:50 +0600

    job category list added

--- src/Categorycard/Categorycard.jsx
+++ src/Categorycard/Categorycard.jsx
@@ -3,8 +3,18 @@ import './Categorycard.css';
 
 const Categorycard = ({category}) => {
     // console.log(category)
+    const {logo,availability,category_name} =category;
     return (
-        <div>
+        <div className="Categorycard">
+            <div className="CategorycardImage ">
+            <img src={logo} alt=""/>
+            </div>
+            <div className="categarory-items">
+                <h3>
+                    {category_name}
+                </h3>
+                <p>{availability}</p>
+            </div>
             
         </div>
     );
--- src/Jobcategories/Jobcategories.jsx
+++ src/Jobcategories/Jobcategories.jsx
@@ -7,12 +7,18 @@ const Jobcategories = ({categories}) => {
     // console.log(categories);
     return (
         <section className="jobcategories">
+            <div className="jobcategory-title">
+                <h2>Job Category List</h2>
+                <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+            <div className="categories">
             {
               categories.map(category=><Categorycard
               key={category.id}
               category={category}
               ></Categorycard>)
             }
+            </div>
         </section>
     );
 };
